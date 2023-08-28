94c028b3b5fb1bacbd024792b5d5aa3aff1180f5
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-08-28 23:42:29 +0300

    Define model relations

--- models/index.js
+++ models/index.js
@@ -6,3 +6,26 @@ const { Review } = require("./reviews");
 const { Section } = require("./sections");
 const { User } = require("./users");
 
+User.belongsTo(Section);
+Section.hasMany(User);
+
+Request.belongsTo(User);
+User.hasMany(Request);
+
+Offer.belongsTo(Request);
+Request.hasMany(Offer);
+
+Offer.belongsTo(User);
+User.hasMany(Offer);
+
+Review.belongsTo(Request);
+Request.hasOne(Review);
+
+Review.belongsTo(User);
+User.hasMany(Review);
+
+Report.belongsTo(User, { as: 'sender', foreignKey: 'senderId' });
+Report.belongsTo(User, { as: 'receiver', foreignKey: 'receiverId' });
+
+User.hasMany(Report, { as: 'sentReports', foreignKey: 'senderId' });
+User.hasMany(Report, { as: 'receivedReports', foreignKey: 'receiverId' });
