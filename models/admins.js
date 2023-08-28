106cca2c2cb29c208875117a0489b1e48ae75df3
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-08-29 00:38:53 +0300

    Implement Admin Helper functions

--- models/admins.js
+++ models/admins.js
@@ -20,3 +20,53 @@ exports.Admin = sequelize.define("Admin", {
         allowNull: false,
     }
 })
+
+exports.getAdminByUsername = async username => {
+    return await this.Admin.findOne({ where: { username } });
+}
+
+exports.createAdmin = async (username, name, password) => {
+    let data = { name, username }
+    data.password = await bcrypt.hash(password, 10);
+    return await this.Admin.create(data);
+}
+
+exports.verifyPassword = async (user, password) => {
+    return await bcrypt.compare(password, user.password);
+}
+
+exports.generateToken = user => {
+    return jwt.sign({
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        role: 1,
+    }, config.secret);
+}
+
+exports.verifyToken = token => {
+    try {
+        let data = jwt.verify(token, config.secret)
+        if (data.role === 1) return data;
+        return false;
+    } catch (err) {
+        return false;
+    }
+}
+
+exports.getAllAdmins = async () => {
+    return await this.Admin.findAll({ attributes: ["id", "username", "name"] });
+}
+
+exports.updateAdmin = async (adminId, name, username, password) => {
+    let data = {};
+    name && (data.name = name);
+    username && (data.username = username);
+    password && (data.password = await bcrypt.hash(password, 10));
+
+    return await this.Admin.update(data, { where: { id: adminId } });
+}
+
+exports.deleteAdmin = async adminId => {
+    return await this.Admin.destroy({ where: { id: adminId } });
+}
