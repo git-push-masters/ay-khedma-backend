bfcf940599d23ed5cb9405daf7382a27c60600da
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-08-28 23:37:51 +0300

    Create Review Model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const { Admin } = require("./admins");
 const { Offer } = require("./offers");
 const { Request } = require("./requests");
+const { Review } = require("./reviews");
 const { Section } = require("./sections");
 const { User } = require("./users");
 
