d99e98d2c7d522f1fa5d0e6bef69e66b53d5d6a4
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-08-29 00:24:47 +0300

    Implement User Helper functions

--- models/users.js
+++ models/users.js
@@ -1,5 +1,9 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config");
+const { Section } = require('./sections');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 exports.User = sequelize.define('User', {
     id: {
@@ -59,3 +63,50 @@ exports.User = sequelize.define('User', {
         defaultValue: false
     }
 });
+
+exports.getUserByPhone = async (phone) => {
+    return await this.User.findOne({ where: { phone } });
+}
+
+exports.getUserById = async (userId) => {
+    return await this.User.findByPk(userId, { include: Section });
+}
+
+exports.createUser = async (name, phone, email, password, avatar, identity, address, bio, locationLat, locationLong, isPhoneVisible, isEmailVisible, isLocationVisible) => {
+    let data = { name, phone };
+    data.password = await bcrypt.hash(password, 10);
+    email && (data.email = email);
+    avatar && (data.avatar = avatar);
+    identity && (data.identity = identity);
+    address && (data.address = address);
+    bio && (data.bio = bio);
+    locationLat && (data.locationLat = locationLat);
+    locationLong && (data.locationLong = locationLong);
+    isPhoneVisible && (data.isPhoneVisible = isPhoneVisible);
+    isEmailVisible && (data.isEmailVisible = isEmailVisible);
+    isLocationVisible && (data.isLocationVisible = isLocationVisible);
+    return await this.User.create(data);
+}
+
+exports.verifyPassword = async (user, password) => {
+    return await bcrypt.compare(password, user.password);
+}
+
+exports.generateToken = (user) => {
+    return jwt.sign({
+        id: user.id,
+        name: user.name,
+        phone: user.phone,
+        role: 0
+    }, config.secret);
+}
+
+exports.verifyToken = (token) => {
+    try {
+        let data = jwt.verify(token, config.secret);
+        if (data.role === 0) return data;
+        return false;
+    } catch (err) {
+        return false;
+    }
+}
