4f930ac204be87a47b32d17de04808396b8a5618
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-09-02 05:41:14 +0300

    Implement Request helper functions

--- models/request.js
+++ models/request.js
@@ -1,6 +1,8 @@
 "use strict"
 const { Model } = require("sequelize")
 module.exports = (sequelize, DataTypes) => {
+    const Section = require('./section')(sequelize, DataTypes);
+
     class Request extends Model {
         get thumbnails() {
             return JSON.parse(this.getDataValue("thumbnailsText"))
@@ -68,6 +70,49 @@ module.exports = (sequelize, DataTypes) => {
             modelName: "Request",
         }
     )
+
+    Request.getRequests = async (status = 1, sectionId, locationLat, locationLong, page = 1, limit = 10) => {
+        let options = { where: { status }, limit, offset: (page - 1) * limit };
+        sectionId && (options.where.sectionId = sectionId);
+        let distanceField = sequelize.literal(`sqrt(pow(${locationLat} - "locationLat", 2) + pow(${locationLong} - "locationLong", 2))`);
+        if (locationLat && locationLong) {
+            options.attributes = ["id", "title", "address", "description", "minPrice", "maxPrice", "durationRange", "locationLat", "locationLong", [distanceField, "distance"]];
+            options.order = ["distance ASC"];
+        }
+        return await Request.findAll(options);
+    }
+
+    Request.getRequestById = async (requestId) => {
+        return await Request.findByPk(requestId, { include: Section });
+    }
+
+    Request.createRequest = async (title, address, description, thumbnails, sectionId, minPrice, maxPrice, durationRange, locationLat, locationLong) => {
+        let data = { title, sectionId, maxPrice };
+        address && (data.address = address);
+        description && (data.description = description);
+        thumbnails && (data.thumbnails = thumbnails);
+        minPrice && (data.minPrice = minPrice);
+        durationRange && (data.durationRange = durationRange);
+        locationLat && (data.locationLat = locationLat);
+        locationLong && (data.locationLong = locationLong);
+        return await Request.create(data);
+    }
+
+    Request.updateRequest = async (requestId, title, address, description, thumbnails, sectionId, minPrice, maxPrice, durationRange, locationLat, locationLong) => {
+        let data = { title, sectionId, maxPrice };
+        address && (data.address = address);
+        description && (data.description = description);
+        thumbnails && (data.thumbnails = thumbnails);
+        minPrice && (data.minPrice = minPrice);
+        durationRange && (data.durationRange = durationRange);
+        locationLat && (data.locationLat = locationLat);
+        locationLong && (data.locationLong = locationLong);
+        return await Request.update(data, { where: { id: requestId } });
+    }
+
+    Request.deleteRequest = async (requestId) => {
+        return await Request.destroy({ where: { id: requestId } });
+    }
 
     return Request
 }
