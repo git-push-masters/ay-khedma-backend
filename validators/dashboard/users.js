2c272e5cfab704c61e0df17c0dcb5a7a68c2b3a4
zayat.ace <zayat.ace@gmail.com>
2023-09-09 19:03:32 +0300

    dashboard: users validators

--- validators/dashboard/users.js
+++ validators/dashboard/users.js
@@ -1,21 +1,59 @@
-const requireValidation = require('..');
+const { param, check } = require("express-validator");
+const sectionsModel = require("../../models").Section;
+const requireValidation = require("..");
 
-exports.getUsers = [
-    /** @TODO Insert Validators */
-    requireValidation
-]
+exports.getUsers = [requireValidation];
 
 exports.getUser = [
-    /** @TODO Insert Validators */
-    requireValidation
-]
+    param("userId").notEmpty().withMessage("يجب إدخال رقم المستخدم"),
+    requireValidation,
+];
 
 exports.patchUser = [
-    /** @TODO Insert Validators */
-    requireValidation
-]
+    param("userId").notEmpty().withMessage("يجب إدخال رقم المستخدم"),
+    check("phone")
+        .optional()
+        .isLength({ min: 11, max: 11 })
+        .withMessage("يجب أن يتكون رقم الهاتف من 11 رقم"),
+    check("email")
+        .optional()
+        .isEmail()
+        .withMessage("البريد الإلكتروني غير صالح"),
+    check("locationLat").optional().isFloat().withMessage("الموقع غير صالح"),
+    check("locationLong").optional().isFloat().withMessage("الموقع غير صالح"),
+    check("isPhoneVisible")
+        .optional()
+        .isBoolean()
+        .withMessage("تأكد من اختيار اذا كنت تريد أن يظهر رقمك للعامة"),
+    check("isEmailVisible")
+        .optional()
+        .isBoolean()
+        .withMessage(
+            "تأكد من اختيار اذا كنت تريد أن يظهر بريدك الإلكتروني للعامة"
+        ),
+    check("isLocationVisible")
+        .optional()
+        .isBoolean()
+        .withMessage("تأكد من اختيار اذا كنت تريد أن يظهر موقعك للعامة"),
+    check("avatar").optional().isURL().withMessage("عنوان الصورة غير صالح"),
+    check("identity")
+        .optional()
+        .isURL()
+        .withMessage("عنوان الهوية الشخصية غير صالح"),
+    check("sectionId")
+        .optional()
+        .isInt()
+        .withMessage("القسم غير صالح")
+        .bail()
+        .custom(async sectionId => {
+            let section = await sectionsModel.getSectionById(sectionId);
+            if (!section) return Promise.reject("القسم غير موجود");
+            Promise.resolve();
+        }),
+    requireValidation,
+];
 
 exports.deleteUser = [
-    /** @TODO Insert Validators */
-    requireValidation
-]
+    param("userId").notEmpty().withMessage("يجب إدخال رقم المستخدم"),
+    requireValidation,
+];
