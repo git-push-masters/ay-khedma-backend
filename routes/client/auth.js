d2507f07930ec3c6c4f2b37b55cd8da1e2d84f86
zayat.ace <zayat.ace@gmail.com>
2023-09-12 23:37:57 +0300

    add me route

--- routes/client/auth.js
+++ routes/client/auth.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const ctrl = require('../../controllers/client/auth');
-const validator = require('../../validators/client/auth');
+const ctrl = require("../../controllers/client/auth");
+const { requireClient } = require("../../permissions/auth");
+const validator = require("../../validators/client/auth");
 
-router.post('/login', ...validator.login, ctrl.login);
-router.post('/register', ...validator.register, ctrl.register);
-router.post('/verify', ...validator.verify, ctrl.verify);
+router.post("/login", ...validator.login, ctrl.login);
+router.post("/register", ...validator.register, ctrl.register);
+router.post("/verify", ...validator.verify, ctrl.verify);
+router.get("/me", requireClient, ctrl.me);
 
 module.exports = router;
