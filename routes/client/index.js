6ac2437913e57305a84aa5a75945e12dce9feace
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-09-12 14:44:03 +0300

    Disable protecting route for client sections

--- routes/client/index.js
+++ routes/client/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const { requireClient } = require("../../permissions/auth");
 
 router.use('/auth', require('./auth'));
+router.use('/sections', require('./sections'));
 
 router.use(requireClient);
 
 router.use('/users', require('./users'));
-router.use('/sections', require('./sections'));
 router.use('/requests', require('./requests'));
 
 module.exports = router;
--- routes/client/sections.js
+++ routes/client/sections.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 
 const ctrl = require('../../controllers/client/sections');
 const validator = require('../../validators/client/sections');
+const { requireClient } = require('../../permissions/auth');
 
 router.get('/', ctrl.getSections);
 router.get('/:sectionId', ...validator.getSection, ctrl.getSection);
-router.get('/:sectionId/requests', ...validator.getSectionRequests, ctrl.getSectionRequests);
+router.get(requireClient, '/:sectionId/requests', ...validator.getSectionRequests, ctrl.getSectionRequests);
 
 module.exports = router;
