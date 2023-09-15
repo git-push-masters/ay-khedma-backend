8285a54cfe09d9737846133ff9be22f34eaec890
zayat.ace <zayat.ace@gmail.com>
2023-09-16 01:52:33 +0300

    requests controller in dashboard

--- controllers/dashboard/requests.js
+++ controllers/dashboard/requests.js
@@ -1,32 +1,59 @@
-const requestsModel = require('../../models').Request;
-
+const requestsModel = require("../../models").Request;
 
 /** @type {import("express").RequestHandler} */
 exports.getRequests = async (req, res, next) => {
-    /** @TODO Implement Controller */
-}
+    try {
+        let { page = 1 } = req.query;
+        let requests = await requestsModel.getRequests(req.query);
+        res.status(200).json({
+            success: true,
+            status: 200,
+            msgs: [],
+            count: requests.length,
+            page,
+            body: requests,
+        });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+};
 
 /** @type {import("express").RequestHandler} */
 exports.getRequest = async (req, res, next) => {
     /** @TODO Implement Controller */
-}
+};
 
 /** @type {import("express").RequestHandler} */
 exports.deleteRequest = async (req, res, next) => {
-    /** @TODO Implement Controller */
-}
+    try {
+        let request = await requestsModel.getRequestById(req.params.requestId);
+        if (!request) return next({ status: 404, msgs: ["الطلب غير موجود"] });
+
+        await requestsModel.deleteRequest(req.params.requestId);
+
+        res.status(200).json({
+            success: true,
+            status: 200,
+            msgs: [],
+        });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+};
 
 /** @type {import("express").RequestHandler} */
 exports.getRequestOffers = async (req, res, next) => {
     /** @TODO Implement Controller */
-}
+};
 
 /** @type {import("express").RequestHandler} */
 exports.getRequestOffer = async (req, res, next) => {
     /** @TODO Implement Controller */
-}
+};
 
 /** @type {import("express").RequestHandler} */
 exports.getRequestReview = async (req, res, next) => {
     /** @TODO Implement Controller */
-}
+};
